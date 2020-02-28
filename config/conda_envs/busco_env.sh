0955885cab36dcb98a3b4096d08860a4a15971fb
ac7ac5d <acormier@ifremer.fr>
2020-02-28 15:58:28 +0000

    Busco 4.0.0 to 4.0.4 + export Augustus config path

--- config/conda_envs/busco_env.sh
+++ config/conda_envs/busco_env.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-export PATH=/appli/anaconda/3.7/bin/:$PATH
-source activate /appli/conda-env/bioinfo/busco-4.0.0
+export PATH=$PATH:/appli/anaconda/latest/bin
+source activate /appli/conda-env/bioinfo/busco-4.0.4
+if [ ! -d $HOME/augustus_config/ ]
+then
+        mkdir -p $HOME/augustus_config/
+        cp -r /appli/conda-env/bioinfo/busco-4.0.4/config/* $HOME/augustus_config/
+fi
+export AUGUSTUS_CONFIG_PATH=$HOME/augustus_config/
