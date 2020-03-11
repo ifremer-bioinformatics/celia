25e1a8fd5a9a7023d1c3659b02d382ab0ae73aeb
ac7ac5d <acormier@ifremer.fr>
2020-03-11 08:45:44 +0000

    mv tmpdir + java memory

--- RunCELIA.sh
+++ RunCELIA.sh
@@ -6,10 +6,10 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . ${BASEDIR}/config/conda_envs/nextflow_env.sh
 
 #nextflow temp directory
-export NXF_TEMP=$DATAWORK/nxt-tmp
+export NXF_TEMP=$DATAWORK/nextflow/nxt-tmp
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)
-nextflow -DXms=1G -DXmx=4G -trace nextflow.executor run CELIA.nf $1
+nextflow -DXms=2G -DXmx=8G -trace nextflow.executor run CELIA.nf $1
 
 #deactivate nextflow environment
 . ${BASEDIR}/config/conda_envs/delenv.sh
